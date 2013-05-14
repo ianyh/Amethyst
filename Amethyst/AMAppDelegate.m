9df9236c53e74c599a7be7e523a7d3979e185c97
Ian Ynda-Hummel <ianynda@gmail.com>
2013-05-14 17:46:42 -0400

    Set up app as a status bar app.

--- Amethyst/AMAppDelegate.m
+++ Amethyst/AMAppDelegate.m
@@ -8,11 +8,24 @@
 
 #import "AMAppDelegate.h"
 
+@interface AMAppDelegate ()
+@property (nonatomic, strong) NSStatusItem *statusItem;
+@property (nonatomic, strong) IBOutlet NSMenu *statusItemMenu;
+@end
+
 @implementation AMAppDelegate
 
-- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
-{
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
     // Insert code here to initialize your application
+}
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+
+    self.statusItem = [[NSStatusBar systemStatusBar] statusItemWithLength:NSVariableStatusItemLength];
+    [self.statusItem setTitle:@"AM"];
+    [self.statusItem setMenu:self.statusItemMenu];
+    [self.statusItem setHighlightMode:YES];
 }
 
 @end
