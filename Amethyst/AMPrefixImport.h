d9deabc801109101f12a87f79148e54f0390afa8
Ian Ynda-Hummel <ianynda@gmail.com>
2013-07-26 18:37:06 -0400

    Factor out a global prefix import file.

---
+++ Amethyst/AMPrefixImport.h
@@ -0,0 +1,14 @@
+//
+//  AMPrefixImport.h
+//  Amethyst
+//
+//  Created by Ian Ynda-Hummel on 7/26/13.
+//  Copyright (c) 2013 Ian Ynda-Hummel. All rights reserved.
+//
+
+#ifndef Amethyst_AMPrefixImport_h
+#define Amethyst_AMPrefixImport_h
+
+#import <libextobjc/EXTSelectorChecking.h>
+
+#endif
