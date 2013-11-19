be25f53cc35b800d0b77dc4a9e170dcdf0590874
Ian Ynda-Hummel <ianynda@gmail.com>
2013-11-18 21:16:32 -0500

    Fix up styling of layout name windows.

--- Amethyst/AMLayoutNameWindow.m
+++ Amethyst/AMLayoutNameWindow.m
@@ -22,11 +22,18 @@
 
     [self setOpaque:NO];
     [self setIgnoresMouseEvents:YES];
-//    [self setBackgroundColor:[NSColor clearColor]];
+    [self setBackgroundColor:[NSColor clearColor]];
     [self setLevel:NSFloatingWindowLevel];
+}
 
-    self.containerView.layer.cornerRadius = 12;
-    self.containerView.layer.backgroundColor = NSColor.blackColor.CGColor;
+- (void)setContentView:(NSView *)aView {
+    aView.wantsLayer = YES;
+    aView.layer.frame = aView.frame;
+    aView.layer.cornerRadius = 20.0;
+    aView.layer.masksToBounds = YES;
+    aView.layer.backgroundColor = [[NSColor blackColor] colorWithAlphaComponent:0.75].CGColor;
+
+    [super setContentView:aView];
 }
 
 @end
