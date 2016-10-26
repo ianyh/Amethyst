c833746847334481c43b03f0800f7abc14c0c304
Glib Smaga <glibsm@gmail.com>
2016-10-25 23:59:12 -0700

    Get the main pane thingy working for Row layout

--- Amethyst/Layout/RowLayout.swift
+++ Amethyst/Layout/RowLayout.swift
@@ -21,17 +21,41 @@ private class RowReflowOperation: ReflowOperation {
             return
         }
 
+        let mainPaneCount = min(windows.count, layout.mainPaneCount)
+        let secondaryPaneCount = windows.count - mainPaneCount
+        let hasSecondaryPane = secondaryPaneCount > 0
+
         let screenFrame = adjustedFrameForLayout(screen)
-        let windowHeight = screenFrame.height / CGFloat(windows.count)
+
+        let mainPaneWindowHeight = round(screenFrame.size.height * (hasSecondaryPane ? CGFloat(layout.mainPaneRatio) : 1.0))
+        let secondaryPaneWindowHeight = hasSecondaryPane ? round((screenFrame.size.height - mainPaneWindowHeight) / CGFloat(secondaryPaneCount)) : 0.0
 
         let focusedWindow = SIWindow.focusedWindow()
 
         let frameAssignments = windows.reduce([]) { frameAssignments, window -> [FrameAssignment] in
             var assignments = frameAssignments
-            let originY = screenFrame.origin.y + CGFloat(frameAssignments.count) * windowHeight
-            let windowFrame = CGRect(x: screenFrame.origin.x, y: originY, width: screenFrame.width, height: windowHeight)
+            var windowFrame = CGRect.zero
 
-            let frameAssignment = FrameAssignment(frame: windowFrame, window: window, focused: window.isEqualTo(focusedWindow), screenFrame: screenFrame)
+            // let windowFrame = CGRect(x: screenFrame.origin.x, y: originY, width: screenFrame.width, height: windowHeight)
+
+            if frameAssignments.count < mainPaneCount {
+                windowFrame.origin.x = screenFrame.origin.x
+                windowFrame.origin.y = screenFrame.origin.y + (mainPaneWindowHeight * CGFloat(frameAssignments.count))
+                windowFrame.size.width = screenFrame.width
+                windowFrame.size.height = mainPaneWindowHeight
+            } else {
+                windowFrame.origin.x = screenFrame.origin.x
+                windowFrame.origin.y = screenFrame.origin.y + mainPaneWindowHeight + (secondaryPaneWindowHeight * CGFloat(frameAssignments.count - mainPaneCount))
+                windowFrame.size.width = screenFrame.width
+                windowFrame.size.height = secondaryPaneWindowHeight
+            }
+
+            let frameAssignment = FrameAssignment(
+                frame: windowFrame,
+                window: window,
+                focused: window.isEqualTo(focusedWindow),
+                screenFrame: screenFrame
+            )
 
             assignments.append(frameAssignment)
 
@@ -46,11 +70,31 @@ private class RowReflowOperation: ReflowOperation {
     }
 }
 
+// TODO(glib): there is quite a lot of this copied around between Layouts... probably should abstract
 public class RowLayout: Layout {
     override public class var layoutName: String { return "Row" }
     override public class var layoutKey: String { return "row" }
 
+    private var mainPaneCount: Int = 1
+    private var mainPaneRatio: CGFloat = 0.5
+
     override public func reflowOperationForScreen(screen: NSScreen, withWindows windows: [SIWindow]) -> ReflowOperation {
         return RowReflowOperation(screen: screen, windows: windows, layout: self, windowActivityCache: windowActivityCache)
+    }
+
+    override public func expandMainPane() {
+        mainPaneRatio = max(0, mainPaneRatio + 0.05)
+    }
+
+    override public func shrinkMainPane() {
+        mainPaneRatio = max(0, mainPaneRatio - 0.05)
+    }
+
+    override public func increaseMainPaneCount() {
+        mainPaneCount = mainPaneCount + 1
+    }
+
+    override public func decreaseMainPaneCount() {
+        mainPaneCount = max(1, mainPaneCount - 1)
     }
 }
