e43be3f2bc3798893ce5e980a9f5c7fe8dd9d798
Don Patterson <d_j_p_3@djp3.net>
2017-01-03 23:47:27 -0800

    Trying to satisfy the insatiable houndci-bot

--- Amethyst/Layout/AutoBinarySpacePartitioningLayout.swift
+++ Amethyst/Layout/AutoBinarySpacePartitioningLayout.swift
@@ -28,46 +28,46 @@ private class AutoBinarySpacePartitioningReflowOperation: ReflowOperation {
         binaryFrames.append(screenFrame)
 
         // Split until we have the right number of frames to hold the windows
-        while(binaryFrames.count < windows.count){
+        while binaryFrames.count < windows.count {
             //Find the frame with the largest area and then split it
-            var largestArea:Float = -1.0
+            var largestArea: Float = -1.0
             var largestAreaIndex = -1
             for index in 0...(binaryFrames.count-1) {
-                let candidate:CGRect = binaryFrames[index]
+                let candidate: CGRect = binaryFrames[index]
 
                 let area = Float(candidate.size.width) * Float(candidate.size.height)
-                if(area >= largestArea){ //Prefer later matches
+                if area >= largestArea { //Prefer later matches
                     largestArea = area
                     largestAreaIndex = index
                 }
             }
 
             //Sanity check solution
-            if(largestAreaIndex < 0){
+            if largestAreaIndex < 0  {
                 NSLog("Unable to find a window to split: Index of window of -1 is invalid")
                 return
             }
-            if(largestArea <= 0){
+            if largestArea <= 0  {
                 NSLog("Unable to find a window to split: Largest window area of < 1 is invalid")
                 return
             }
 
             //Calculate two child frames
             let splittableFrame = binaryFrames[largestAreaIndex]
-            var childFrame1:CGRect
-            var childFrame2:CGRect
+            var childFrame1: CGRect
+            var childFrame2: CGRect
 
             //Figure out how to split the frame
-            if(splittableFrame.size.width > splittableFrame.size.height){
+            if splittableFrame.size.width > splittableFrame.size.height  {
                 //split vertically x | x
                 let newWidth1 = floor(splittableFrame.size.width * layout.mainPaneRatio)
                 let newWidth2 = ceil(splittableFrame.size.width - newWidth1)
                 let newHeight1 = floor(splittableFrame.size.height)
                 let newHeight2 = ceil(splittableFrame.size.height)
                 childFrame1 = CGRect(x:splittableFrame.origin.x, y:splittableFrame.origin.y, width:newWidth1, height:newHeight1)
-                childFrame2 = CGRect(x:(splittableFrame.origin.x + newWidth1), y:splittableFrame.origin.y,width:newWidth2, height:newHeight2)
+                childFrame2 = CGRect(x:(splittableFrame.origin.x + newWidth1), y:splittableFrame.origin.y, width:newWidth2, height:newHeight2)
             }
-            else{
+            else {
                 //split horizontally  x
                 //                    -
                 //                    x
@@ -133,4 +133,3 @@ open class AutoBinarySpacePartitioningLayout: Layout {
         mainPaneCount = max(1, mainPaneCount - 1)
     }
 }
-
