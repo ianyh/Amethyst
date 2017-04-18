e8c47cf442c92809f982c6cd881f7e0cf89ee9f3
Ian Ynda-Hummel <ianynda@gmail.com>
2017-04-18 19:38:31 -0400

    Use the correct screen count range for commands in preferences
    Fixes #566

---
+++ AmethystTests/Managers/HotKeyManagerTests.swift
@@ -0,0 +1,23 @@
+//
+//  HotKeyManagerTests.swift
+//  Amethyst
+//
+//  Created by Ian Ynda-Hummel on 4/18/17.
+//  Copyright © 2017 Ian Ynda-Hummel. All rights reserved.
+//
+
+@testable import Amethyst
+import Nimble
+import Quick
+
+final class HotKeyManagerTests: QuickSpec {
+    override func spec() {
+        describe("hotKeyNameToDefaultsKey") {
+            it("respects screen count") {
+                let keyMapping = HotKeyManager.hotKeyNameToDefaultsKey(screenCount: 4)
+                let screenCommands = keyMapping.filter { $0[1].hasPrefix(CommandKey.FocusScreenPrefix.rawValue) }
+                expect(screenCommands.count).to(equal(4))
+            }
+        }
+    }
+}
