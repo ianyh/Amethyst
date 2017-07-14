9e07abad38c2aaec1f6320dfcd1e0a50e83b22b3
Ian Katz <ianfixes@gmail.com>
2017-07-14 08:42:30 -0400

    check Xcode version, don't check for Carthage, relax rbenv

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
-# Check for Carthage
-printf "Checking for Carthage: "
-carthage version >/dev/null 2>&1 || { printf >&2 "nope!\nCarthage must be installed. Try running:\n\tbrew install carthage\n"; exit 1; }
-printf "found!\n"
+# Check Xcode version (TODO: semver)
+OUR_XCODE=9.0
+printf "Checking if xcode-select points to the Xcode version we use ($OUR_XCODE): "
+if [ `xcodebuild -version | grep Xcode | awk '{print $2}'` = $OUR_XCODE ] ; then
+    printf "found!\n"
+else
+    printf "nope!\n"
+    printf "WARNING: Xcode $OUR_XCODE is used to develop this project.\n"
+fi
 
 # Check for swiftlint
 printf "Checking for swiftlint: "
@@ -12,11 +17,23 @@ printf "found!\n"
 
 # Check for installed ruby version
 printf "Checking for rbenv: "
-rbenv version >/dev/null 2>&1 || { printf >&2 "nope!\nrbenv must be installed. Try running:\n\tbrew install rbenv\n"; exit 1; }
-printf "found!\n"
-printf "Ensuring correct ruby version is installed:\n"
-rbenv install -s
-printf "Correct ruby version installed!\n"
+if rbenv version >/dev/null 2>&1 ; then
+    printf "found!\n"
+    printf "Ensuring correct ruby version is installed:\n"
+    rbenv install -s
+    printf "Correct ruby version installed!\n"
+
+else
+    printf "nope!\n"
+    ACTUAL=`ruby -v | awk '{print $2}' | awk -Fp '{print $1}' | tr -d '\n'`
+    EXPECTED=`cat .ruby-version | tr -d '\n'`
+    if [ $ACTUAL = $EXPECTED ] ; then
+	printf "Correct ruby version is already installed, without rbenv\n"
+    else
+	printf "WARNING: You have ruby $ACTUAL, we want ruby $EXPECTED, and and rbenv is not installed.\n"
+	printf "If you encounter setup problems, Try running:\n\tbrew install rbenv\n"
+    fi
+fi
 
 # Install CocoaPods via bundler
 printf "Checking for bundler: "
@@ -28,9 +45,5 @@ bundle exec pod --version >/dev/null 2>&1 || { echo >&2 "CocoaPods failed to ins
 # Install pods
 printf "Installing Pods:\n"
 bundle exec pod install
-
-# Build Carthage dependencies
-printf "Building Carthage frameworks:\n"
-carthage bootstrap --platform mac --cache-builds
 
 printf "\n\nDone! Now you can open Amethyst.xcworkspace and build\n"
