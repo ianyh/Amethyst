4404c5ebd4f976edc500ea210e3bc15df4786bcf
Ian Ynda-Hummel <ianynda@gmail.com>
2017-07-16 12:35:17 -0400

    Allow for commands that are not configured by a configuration file
    Fixes #603

--- Amethyst/Managers/HotKeyRegistrar.swift
+++ Amethyst/Managers/HotKeyRegistrar.swift
@@ -11,11 +11,11 @@ import Log
 import MASShortcut
 
 protocol HotKeyRegistrar {
-    func registerHotKey(with string: String, modifiers: AMModifierFlags, handler: @escaping () -> Void, defaultsKey: String, override: Bool)
+    func registerHotKey(with string: String?, modifiers: AMModifierFlags?, handler: @escaping () -> Void, defaultsKey: String, override: Bool)
 }
 
 extension HotKeyManager: HotKeyRegistrar {
-    func registerHotKey(with string: String, modifiers: AMModifierFlags, handler: @escaping () -> Void, defaultsKey: String, override: Bool) {
+    func registerHotKey(with string: String?, modifiers: AMModifierFlags?, handler: @escaping () -> Void, defaultsKey: String, override: Bool) {
         let userDefaults = UserDefaults.standard
 
         if userDefaults.object(forKey: defaultsKey) != nil && !override {
@@ -23,17 +23,19 @@ extension HotKeyManager: HotKeyRegistrar {
             return
         }
 
-        guard let keyCodes = stringToKeyCodes[string.lowercased()], keyCodes.count > 0 else {
-            LogManager.log?.warning("String \"\(string)\" does not map to any keycodes")
-            return
+        // If a command is specified, set it as the default shortcut
+        if let string = string, let modifiers = modifiers {
+            if let keyCodes = stringToKeyCodes[string.lowercased()], keyCodes.count > 0 {
+                let shortcut = MASShortcut(keyCode: UInt(keyCodes[0]), modifierFlags: modifiers)
+                MASShortcutBinder.shared().registerDefaultShortcuts([ defaultsKey: shortcut as Any ])
+            } else {
+                LogManager.log?.warning("String \"\(string)\" does not map to any keycodes")
+            }
         }
 
-        let shortcut = MASShortcut(keyCode: UInt(keyCodes[0]), modifierFlags: modifiers)
-
-        MASShortcutBinder.shared().registerDefaultShortcuts([ defaultsKey: shortcut as Any ])
         MASShortcutBinder.shared().bindShortcut(withDefaultsKey: defaultsKey, toAction: handler)
 
         // Note that the shortcut binder above only sets the default value, not the stored value, so we explicitly store it here.
-        userDefaults.set(userDefaults.object(forKey: defaultsKey), forKey:defaultsKey)
+        userDefaults.set(userDefaults.object(forKey: defaultsKey), forKey: defaultsKey)
     }
 }
