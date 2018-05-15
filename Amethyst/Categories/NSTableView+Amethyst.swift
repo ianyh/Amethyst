900a8af8d457d4d473a2917552050ecfb14fca67
James Zaghini <james@zaghini.com>
2018-05-15 12:30:20 +1000

    Fix: Prevent crash when deleting with no row selected
    For layouts & floating tables in preferences

---
+++ Amethyst/Categories/NSTableView+Amethyst.swift
@@ -0,0 +1,15 @@
+//
+//  NSTableView+Amethyst.swift
+//  Amethyst
+//
+//  Created by James Zaghini on 15/5/18.
+//  Copyright © 2018 Ian Ynda-Hummel. All rights reserved.
+//
+
+import Cocoa
+
+extension NSTableView {
+
+    static let noRowSelectedIndex = -1
+
+}
