5994c5a4b95d0f725d6f542a0f71759468a7d66d
Ian Ynda-Hummel <ianynda@gmail.com>
2019-03-09 16:53:01 -0500

    Add a option in settings for displaying layout debug info

---
+++ Amethyst/Preferences/DebugPreferencesViewController.swift
@@ -0,0 +1,12 @@
+//
+//  DebugPreferencesViewController.swift
+//  Amethyst
+//
+//  Created by Ian Ynda-Hummel on 3/9/19.
+//  Copyright © 2019 Ian Ynda-Hummel. All rights reserved.
+//
+
+import AppKit
+import Foundation
+
+class DebugPreferencesViewController: NSViewController {}
