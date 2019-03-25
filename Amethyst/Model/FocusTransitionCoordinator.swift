cbd16d1ea6d868c44496fc902ca62c2b2bf1fa8e
Ian Ynda-Hummel <ianynda@gmail.com>
2019-03-24 21:34:01 -0400

    Add structs that can coordinate window and focus transitions

---
+++ Amethyst/Model/FocusTransitionCoordinator.swift
@@ -0,0 +1,154 @@
+//
+//  FocusTransitionCoordinator.swift
+//  Amethyst
+//
+//  Created by Ian Ynda-Hummel on 3/24/19.
+//  Copyright © 2019 Ian Ynda-Hummel. All rights reserved.
+//
+
+import Cocoa
+import Foundation
+import Silica
+
+enum FocusTransition<Window: WindowType> {
+    case focusWindow(_ window: Window)
+    case focusScreen(_ screen: NSScreen)
+}
+
+protocol FocusTransitionTarget: class {
+    associatedtype Window: WindowType
+
+    func executeTransition(_ transition: FocusTransition<Window>)
+
+    func windows(on screen: NSScreen) -> [Window]
+    func lastFocusedWindow(on screen: NSScreen) -> Window?
+    func screen(at index: Int) -> NSScreen?
+    func nextWindowIDClockwise(on screen: NSScreen) -> CGWindowID?
+    func nextWindowIDCounterClockwise(on screen: NSScreen) -> CGWindowID?
+}
+
+class FocusTransitionCoordinator<Target: FocusTransitionTarget> {
+    typealias Window = Target.Window
+
+    weak var target: Target!
+
+    init(target: Target) {
+        self.target = target
+    }
+
+    func moveFocusCounterClockwise() {
+        guard let focusedWindow: Window = Window.currentlyFocused() else {
+            focusScreen(at: 0)
+            return
+        }
+
+        guard let screen = focusedWindow.screen() else {
+            return
+        }
+
+        let windows = target.windows(on: screen)
+
+        guard !windows.isEmpty else {
+            return
+        }
+
+        let windowToFocus = { () -> Window in
+            if let nextWindowID = self.target.nextWindowIDCounterClockwise(on: screen) {
+                let windowToFocusIndex = windows.index { $0.windowID() == nextWindowID } ?? 0
+                return windows[windowToFocusIndex]
+            } else {
+                let windowIndex = windows.index(of: focusedWindow) ?? 0
+                let windowToFocusIndex = (windowIndex == 0 ? windows.count - 1 : windowIndex - 1)
+                return windows[windowToFocusIndex]
+            }
+        }()
+
+        windowToFocus.focus()
+    }
+
+    func moveFocusClockwise() {
+        guard let focusedWindow = Window.currentlyFocused() else {
+            focusScreen(at: 0)
+            return
+        }
+
+        guard let screen = focusedWindow.screen() else {
+            return
+        }
+
+        let windows = target.windows(on: screen)
+
+        guard !windows.isEmpty else {
+            return
+        }
+
+        let windowToFocus = { () -> Window in
+            if let nextWindowID = target.nextWindowIDClockwise(on: screen) {
+                let windowToFocusIndex = windows.index { $0.windowID() == nextWindowID } ?? 0
+                return windows[windowToFocusIndex]
+            } else {
+                let windowIndex = windows.index(of: focusedWindow) ?? windows.count - 1
+                let windowToFocusIndex = (windowIndex + 1) % windows.count
+                return windows[windowToFocusIndex]
+            }
+        }()
+
+        windowToFocus.focus()
+    }
+
+    func moveFocusToMain() {
+        guard let focusedWindow: Window = Window.currentlyFocused() else {
+            focusScreen(at: 0)
+            return
+        }
+
+        guard let screen = focusedWindow.screen() else {
+            return
+        }
+
+        let windows = target.windows(on: screen)
+
+        guard !windows.isEmpty else {
+            return
+        }
+
+        windows[0].focus()
+    }
+
+    func focusScreen(at screenIndex: Int) {
+        guard let screen = target.screen(at: screenIndex) else {
+            return
+        }
+
+        // Do nothing if the screen is already focused
+        if let focusedWindow: Window = Window.currentlyFocused(), let focusedScreen = focusedWindow.screen(), focusedScreen == screen {
+            return
+        }
+
+        // If the previous focus has been tracked, then focus the window that had the focus before.
+        if let previouslyFocused = target.lastFocusedWindow(on: screen), previouslyFocused.isOnScreen() {
+            target.executeTransition(.focusWindow(previouslyFocused))
+            return
+        }
+
+        let windows = target.windows(on: screen)
+
+        // If there are no windows on the screen focus the screen directly
+        guard !windows.isEmpty else {
+            target.executeTransition(.focusScreen(screen))
+            return
+        }
+
+        // Otherwise find the topmost window on the screen
+        let screenCenter = NSPointToCGPoint(NSPoint(x: screen.frame.midX, y: screen.frame.midY))
+
+        // If there is no window at that point just focus the screen directly
+        guard let topWindow = WindowsInformation.topWindowForScreenAtPoint(screenCenter, withWindows: windows) ?? windows.first else {
+            target.executeTransition(.focusScreen(screen))
+            return
+        }
+
+        // Otherwise focus the topmost window
+        target.executeTransition(.focusWindow(topWindow))
+    }
+}
---
+++ Amethyst/Model/WindowTransitionCoordinator.swift
@@ -0,0 +1,186 @@
+//
+//  WindowTransitionCoordinator.swift
+//  Amethyst
+//
+//  Created by Ian Ynda-Hummel on 3/24/19.
+//  Copyright © 2019 Ian Ynda-Hummel. All rights reserved.
+//
+
+import Cocoa
+import Foundation
+import Silica
+
+enum WindowTransition<Window: WindowType> {
+    case addWindow(_ window: Window)
+    case removeWindow(_ window: Window)
+    case switchWindows(_ window1: Window, _ window2: Window)
+    case moveWindowToScreen(_ window: Window, screen: NSScreen)
+    case moveWindowToSpaceAtIndex(_ window: Window, spaceIndex: UInt)
+    case resetFocus
+}
+
+protocol WindowTransitionTarget: class {
+    associatedtype Window: WindowType
+
+    func executeTransition(_ transition: WindowTransition<Window>)
+
+    func activeWindows(on screen: NSScreen) -> [Window]
+    func windowIsFloating(_ window: Window) -> Bool
+    func screen(at index: Int) -> NSScreen?
+    func nextScreenIndexClockwise(from screen: NSScreen) -> Int
+    func nextScreenIndexCounterClockwise(from screen: NSScreen) -> Int
+}
+
+class WindowTransitionCoordinator<Target: WindowTransitionTarget> {
+    typealias Window = Target.Window
+
+    weak var target: Target!
+
+    init(target: Target) {
+        self.target = target
+    }
+
+    func swapFocusedWindowToMain() {
+        guard let focusedWindow: Window = Window.currentlyFocused(), !target.windowIsFloating(focusedWindow), let screen = focusedWindow.screen() else {
+            return
+        }
+
+        let windows = target.activeWindows(on: screen)
+
+        guard let focusedIndex = windows.index(of: focusedWindow) else {
+            return
+        }
+
+        // if there are 2 windows, we can always swap.  Just make sure we don't swap focusedWindow with itself.
+        switch windows.count {
+        case 1:
+            return
+        case 2:
+            target.executeTransition(.switchWindows(focusedWindow, windows[1 - focusedIndex]))
+        default:
+            target.executeTransition(.switchWindows(focusedWindow, windows[0]))
+        }
+    }
+
+    func swapFocusedWindowCounterClockwise() {
+        guard let focusedWindow: Window = Window.currentlyFocused(), !target.windowIsFloating(focusedWindow) else {
+            target.executeTransition(.resetFocus)
+            return
+        }
+
+        guard let screen = focusedWindow.screen() else {
+            return
+        }
+
+        let windows = target.activeWindows(on: screen)
+
+        guard let focusedWindowIndex = windows.index(of: focusedWindow) else {
+            return
+        }
+
+        let windowToSwapWith = windows[(focusedWindowIndex == 0 ? windows.count - 1 : focusedWindowIndex - 1)]
+
+        target.executeTransition(.switchWindows(focusedWindow, windowToSwapWith))
+    }
+
+    func swapFocusedWindowClockwise() {
+        guard let focusedWindow: Window = Window.currentlyFocused(), !target.windowIsFloating(focusedWindow) else {
+            target.executeTransition(.resetFocus)
+            return
+        }
+
+        guard let screen = focusedWindow.screen() else {
+            return
+        }
+
+        let windows = target.activeWindows(on: screen)
+
+        guard let focusedWindowIndex = windows.index(of: focusedWindow) else {
+            return
+        }
+
+        let windowToSwapWith = windows[(focusedWindowIndex + 1) % windows.count]
+
+        target.executeTransition(.switchWindows(focusedWindow, windowToSwapWith))
+        focusedWindow.focus()
+    }
+
+    func throwToScreenAtIndex(_ screenIndex: Int) {
+        guard let screen = target.screen(at: screenIndex), let focusedWindow: Window = Window.currentlyFocused() else {
+            return
+        }
+
+        // If the window is already on the screen do nothing.
+        guard let focusedScreen = focusedWindow.screen(), focusedScreen.screenIdentifier() != screen.screenIdentifier() else {
+            return
+        }
+
+        target.executeTransition(.moveWindowToScreen(focusedWindow, screen: screen))
+    }
+
+    func swapFocusedWindowScreenClockwise() {
+        guard let focusedWindow: Window = Window.currentlyFocused(), !target.windowIsFloating(focusedWindow) else {
+            target.executeTransition(.resetFocus)
+            return
+        }
+
+        guard let screen = focusedWindow.screen() else {
+            return
+        }
+
+        guard let nextScreen = target.screen(at: target.nextScreenIndexClockwise(from: screen)) else {
+            return
+        }
+
+        target.executeTransition(.moveWindowToScreen(focusedWindow, screen: nextScreen))
+    }
+
+    func swapFocusedWindowScreenCounterClockwise() {
+        guard let focusedWindow: Window = Window.currentlyFocused(), !target.windowIsFloating(focusedWindow) else {
+            target.executeTransition(.resetFocus)
+            return
+        }
+
+        guard let screen = focusedWindow.screen() else {
+            return
+        }
+
+        guard let nextScreen = target.screen(at: target.nextScreenIndexCounterClockwise(from: screen)) else {
+            return
+        }
+
+        target.executeTransition(.moveWindowToScreen(focusedWindow, screen: nextScreen))
+    }
+
+    func pushFocusedWindowToSpace(_ space: UInt) {
+        guard let focusedWindow: Window = Window.currentlyFocused(), let _ = focusedWindow.screen() else {
+            return
+        }
+
+        target.executeTransition(.moveWindowToSpaceAtIndex(focusedWindow, spaceIndex: space))
+    }
+
+    func pushFocusedWindowToSpaceLeft() {
+        guard let currentFocusedSpace = SpacesInformation<Window>.currentFocusedSpace(), let spaces = SpacesInformation<Window>.spacesForFocusedScreen() else {
+            return
+        }
+
+        guard let index = spaces.index(of: currentFocusedSpace), index > 0 else {
+            return
+        }
+
+        pushFocusedWindowToSpace(UInt(index))
+    }
+
+    func pushFocusedWindowToSpaceRight() {
+        guard let currentFocusedSpace = SpacesInformation<Window>.currentFocusedSpace(), let spaces = SpacesInformation<Window>.spacesForFocusedScreen() else {
+            return
+        }
+
+        guard let index = spaces.index(of: currentFocusedSpace), index - 2 < spaces.count else {
+            return
+        }
+
+        pushFocusedWindowToSpace(UInt(index + 2))
+    }
+}
