c829cb8af008ae75344cb8a46135a2df792b92d5
Ian Ynda-Hummel <ianynda@gmail.com>
2019-04-13 22:13:41 -0400

    Remove some extraneous files

--- bin/CrashlyticsKeys.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-if [ -f Amethyst/crashlytics_api_key ]; then
-    export CRASHLYTICS_API_KEY="$(cat Amethyst/crashlytics_api_key)"
-fi
-
-if [ -f Amethyst/crashlytics_app_key ]; then
-    export CRASHLYTICS_APP_KEY="$(cat Amethyst/crashlytics_app_key)"
-fi
