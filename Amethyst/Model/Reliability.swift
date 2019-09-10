2329833a59447c0c81271eb26b134fa5a9af0d63
Ian Ynda-Hummel <ianynda@gmail.com>
2019-09-09 20:43:06 -0400

    Add some documentation for reliability

--- Amethyst/Model/Reliability.swift
+++ Amethyst/Model/Reliability.swift
@@ -8,7 +8,13 @@
 
 import Foundation
 
-enum Reliable<T>: Equatable where T: Equatable {
+/// `Relatable` wraps a value with the level of confidence that the value is correct.
+enum Reliable<T> {
+    /// `reliable` means that the value is probably correct.
     case reliable(T)
+
+    /// `unreliable` means that the value may be correct, but the returning function is not confident that the value will remain stable.
     case unreliable(T)
 }
+
+extension Reliable: Equatable where T: Equatable {}
