af94db8642cfb72a9475c2a4f79a8339a5c178a7
Ian Ynda-Hummel <ianynda@gmail.com>
2019-12-18 22:35:10 -0500

    Fix the naming of the widescreen tall layout classes

--- Amethyst/Layout/Layouts/WidescreenTallLayout.swift
+++ Amethyst/Layout/Layouts/WidescreenTallLayout.swift
@@ -90,13 +90,13 @@ extension WidescreenTallLayout: PanedLayout {
     }
 }
 
-class WidescreenTallLayoutRight<Window: WindowType>: WidescreenTallLayout<Window> {
+class WidescreenTallLayoutLeft<Window: WindowType>: WidescreenTallLayout<Window> {
     override class var isRight: Bool { return false }
     override static var layoutName: String { return "Widescreen Tall" }
     override static var layoutKey: String { return "widescreen-tall" }
 }
 
-class WidescreenTallLayoutLeft<Window: WindowType>: WidescreenTallLayout<Window> {
+class WidescreenTallLayoutRight<Window: WindowType>: WidescreenTallLayout<Window> {
     override class var isRight: Bool { return true }
     override static var layoutName: String { return "Widescreen Tall Right" }
     override static var layoutKey: String { return "widescreen-tall-right" }
