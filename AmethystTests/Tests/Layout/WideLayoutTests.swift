f5ea34937a0d72c03661ca3d792b3c6589321d60
Ian Ynda-Hummel <ianynda@gmail.com>
2019-12-18 20:28:41 -0500

    Add tests for wide layout

---
+++ AmethystTests/Tests/Layout/WideLayoutTests.swift
@@ -0,0 +1,197 @@
+//
+//  WideLayoutTests.swift
+//  AmethystTests
+//
+//  Created by Ian Ynda-Hummel on 12/7/19.
+//  Copyright © 2019 Ian Ynda-Hummel. All rights reserved.
+//
+
+@testable import Amethyst
+import Nimble
+import Quick
+import Silica
+
+class WideLayoutTests: QuickSpec {
+    override func spec() {
+        afterEach {
+            TestScreen.availableScreens = []
+        }
+
+        describe("layout") {
+            it("separates into a main pane and a secondary pane") {
+                let screen = TestScreen(frame: CGRect(origin: .zero, size: CGSize(width: 2000, height: 1000)))
+                TestScreen.availableScreens = [screen]
+
+                let windows = [
+                    TestWindow(element: nil)!,
+                    TestWindow(element: nil)!,
+                    TestWindow(element: nil)!
+                ]
+                let layoutWindows = windows.map {
+                    LayoutWindow(id: $0.windowID(), frame: $0.frame(), isFocused: false)
+                }
+                let windowSet = WindowSet<TestWindow>(
+                    windows: layoutWindows,
+                    isWindowWithIDActive: { _ in return true },
+                    isWindowWithIDFloating: { _ in return false },
+                    windowForID: { id in return windows.first { $0.windowID() == id } }
+                )
+                let layout = WideLayout<TestWindow>()
+                let frameAssignments = layout.frameAssignments(windowSet, on: screen)!
+
+                expect(layout.mainPaneCount).to(equal(1))
+
+                let mainAssignment = frameAssignments.forWindows(windows[..<1])
+                let secondaryAssignments = frameAssignments.forWindows(windows[1...])
+
+                mainAssignment.verify(frames: [
+                    CGRect(origin: .zero, size: CGSize(width: 2000, height: 500))
+                ])
+                secondaryAssignments.verify(frames: [
+                    CGRect(x: 0, y: 500, width: 1000, height: 500),
+                    CGRect(x: 1000, y: 500, width: 1000, height: 500)
+                ])
+            }
+
+            it("increases and decreases windows in the main pane") {
+                let screen = TestScreen(frame: CGRect(origin: .zero, size: CGSize(width: 2000, height: 1000)))
+                TestScreen.availableScreens = [screen]
+
+                let windows = [
+                    TestWindow(element: nil)!,
+                    TestWindow(element: nil)!,
+                    TestWindow(element: nil)!,
+                    TestWindow(element: nil)!
+                ]
+                let layoutWindows = windows.map {
+                    LayoutWindow(id: $0.windowID(), frame: $0.frame(), isFocused: false)
+                }
+                let windowSet = WindowSet<TestWindow>(
+                    windows: layoutWindows,
+                    isWindowWithIDActive: { _ in return true },
+                    isWindowWithIDFloating: { _ in return false },
+                    windowForID: { id in return windows.first { $0.windowID() == id } }
+                )
+                let layout = WideLayout<TestWindow>()
+
+                expect(layout.mainPaneCount).to(equal(1))
+
+                var frameAssignments = layout.frameAssignments(windowSet, on: screen)!
+                var mainAssignments = frameAssignments.forWindows(windows[..<1])
+                var secondaryAssignments = frameAssignments.forWindows(windows[1...])
+
+                mainAssignments.verify(frames: [
+                    CGRect(x: 0, y: 0, width: 2000, height: 500)
+                ])
+
+                secondaryAssignments.verify(frames: [
+                    CGRect(x: 0, y: 500, width: 667, height: 500),
+                    CGRect(x: 667, y: 500, width: 667, height: 500),
+                    CGRect(x: 1334, y: 500, width: 667, height: 500)
+                ])
+
+                layout.increaseMainPaneCount()
+                expect(layout.mainPaneCount).to(equal(2))
+
+                frameAssignments = layout.frameAssignments(windowSet, on: screen)!
+                mainAssignments = frameAssignments.forWindows(windows[..<2])
+                secondaryAssignments = frameAssignments.forWindows(windows[2...])
+
+                mainAssignments.verify(frames: [
+                    CGRect(x: 0, y: 0, width: 1000, height: 500),
+                    CGRect(x: 1000, y: 0, width: 1000, height: 500)
+                ])
+
+                secondaryAssignments.verify(frames: [
+                    CGRect(x: 0, y: 500, width: 1000, height: 500),
+                    CGRect(x: 1000, y: 500, width: 1000, height: 500)
+                ])
+
+                layout.increaseMainPaneCount()
+                expect(layout.mainPaneCount).to(equal(3))
+
+                frameAssignments = layout.frameAssignments(windowSet, on: screen)!
+                mainAssignments = frameAssignments.forWindows(windows[..<3])
+                secondaryAssignments = frameAssignments.forWindows(windows[3...])
+
+                mainAssignments.verify(frames: [
+                    CGRect(x: 0, y: 0, width: 667, height: 500),
+                    CGRect(x: 667, y: 0, width: 667, height: 500),
+                    CGRect(x: 1334, y: 0, width: 667, height: 500)
+                ])
+
+                secondaryAssignments.verify(frames: [
+                    CGRect(x: 0, y: 500, width: 2000, height: 500)
+                ])
+            }
+
+            it("changes distribution based on pane ratio") {
+                let screen = TestScreen(frame: CGRect(origin: .zero, size: CGSize(width: 2000, height: 1000)))
+                TestScreen.availableScreens = [screen]
+
+                let windows = [
+                    TestWindow(element: nil)!,
+                    TestWindow(element: nil)!,
+                    TestWindow(element: nil)!
+                ]
+                let layoutWindows = windows.map {
+                    LayoutWindow(id: $0.windowID(), frame: $0.frame(), isFocused: false)
+                }
+                let windowSet = WindowSet<TestWindow>(
+                    windows: layoutWindows,
+                    isWindowWithIDActive: { _ in return true },
+                    isWindowWithIDFloating: { _ in return false },
+                    windowForID: { id in return windows.first { $0.windowID() == id } }
+                )
+                let layout = WideLayout<TestWindow>()
+
+                expect(layout.mainPaneCount).to(equal(1))
+
+                var frameAssignments = layout.frameAssignments(windowSet, on: screen)!
+                var mainAssignments = frameAssignments.forWindows(windows[..<1])
+                var secondaryAssignments = frameAssignments.forWindows(windows[1...])
+
+                mainAssignments.verify(frames: [
+                    CGRect(x: 0, y: 0, width: 2000, height: 500)
+                ])
+
+                secondaryAssignments.verify(frames: [
+                    CGRect(x: 0, y: 500, width: 1000, height: 500),
+                    CGRect(x: 1000, y: 500, width: 1000, height: 500)
+                ])
+
+                layout.recommendMainPaneRatio(0.75)
+                expect(layout.mainPaneRatio).to(equal(0.75))
+
+                frameAssignments = layout.frameAssignments(windowSet, on: screen)!
+                mainAssignments = frameAssignments.forWindows(windows[..<1])
+                secondaryAssignments = frameAssignments.forWindows(windows[1...])
+
+                mainAssignments.verify(frames: [
+                    CGRect(x: 0, y: 0, width: 2000, height: 750)
+                ])
+
+                secondaryAssignments.verify(frames: [
+                    CGRect(x: 0, y: 750, width: 1000, height: 250),
+                    CGRect(x: 1000, y: 750, width: 1000, height: 250)
+                ])
+
+                layout.recommendMainPaneRatio(0.25)
+                expect(layout.mainPaneRatio).to(equal(0.25))
+
+                frameAssignments = layout.frameAssignments(windowSet, on: screen)!
+                mainAssignments = frameAssignments.forWindows(windows[..<1])
+                secondaryAssignments = frameAssignments.forWindows(windows[1...])
+
+                mainAssignments.verify(frames: [
+                    CGRect(x: 0, y: 0, width: 2000, height: 250)
+                ])
+
+                secondaryAssignments.verify(frames: [
+                    CGRect(x: 0, y: 250, width: 1000, height: 750),
+                    CGRect(x: 1000, y: 250, width: 1000, height: 750)
+                ])
+            }
+        }
+    }
+}
