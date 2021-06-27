7a48e29ba823f7e7478bc120c21a628b01aabf42
Michael Wizner <mwz@users.noreply.github.com>
2021-06-27 03:53:49 +0100

    Add TwoPane layout (#1124)
    * Add TwoPane layout
    
    * Bump deps to make it work locally

---
+++ Amethyst/Layout/Layouts/TwoPaneLayout.swift
@@ -0,0 +1,102 @@
+//
+//  TwoPaneLayout.swift
+//  Amethyst
+//
+//  Created by @mwz on 10/06/2021.
+//  Copyright © 2021 Ian Ynda-Hummel. All rights reserved.
+//
+
+import Silica
+
+class TwoPaneLayout<Window: WindowType>: Layout<Window>, PanedLayout {
+    override static var layoutName: String { return "Two Pane" }
+    override static var layoutKey: String { return "two-pane" }
+
+    enum CodingKeys: String, CodingKey {
+        case mainPaneCount
+        case mainPaneRatio
+    }
+
+    override var layoutDescription: String { return "" }
+
+    private(set) var mainPaneCount: Int = 1
+    private(set) var mainPaneRatio: CGFloat = 0.5
+
+    required init() {
+        super.init()
+    }
+
+    required init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        self.mainPaneCount = try values.decode(Int.self, forKey: .mainPaneCount)
+        self.mainPaneRatio = try values.decode(CGFloat.self, forKey: .mainPaneRatio)
+        super.init()
+    }
+
+    override func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(mainPaneCount, forKey: .mainPaneCount)
+        try container.encode(mainPaneRatio, forKey: .mainPaneRatio)
+    }
+
+    func recommendMainPaneRawRatio(rawRatio: CGFloat) {
+        mainPaneRatio = rawRatio
+    }
+
+    func increaseMainPaneCount() {}
+
+    func decreaseMainPaneCount() {}
+
+    override func frameAssignments(_ windowSet: WindowSet<Window>, on screen: Screen) -> [FrameAssignmentOperation<Window>]? {
+        let windows = windowSet.windows
+
+        guard !windows.isEmpty else {
+            return []
+        }
+
+        let mainPaneCount = min(windows.count, self.mainPaneCount)
+        let secondaryPaneCount = windows.count > 1 ? 1 : 0
+        let hasSecondaryPane = secondaryPaneCount > 0
+
+        let screenFrame = screen.adjustedFrame()
+
+        let mainPaneWindowHeight = round(screenFrame.size.height / CGFloat(mainPaneCount))
+        let secondaryPaneWindowHeight = hasSecondaryPane ? round(screenFrame.size.height / CGFloat(secondaryPaneCount)) : 0.0
+
+        let mainPaneWindowWidth = round(screenFrame.size.width * (hasSecondaryPane ? CGFloat(mainPaneRatio) : 1.0))
+        let secondaryPaneWindowWidth = screenFrame.size.width - mainPaneWindowWidth
+
+        return windows.reduce([]) { acc, window -> [FrameAssignmentOperation<Window>] in
+            var assignments = acc
+            var windowFrame = CGRect.zero
+            let isMain = acc.count < mainPaneCount
+            var scaleFactor: CGFloat
+
+            if isMain {
+                scaleFactor = screenFrame.size.width / mainPaneWindowWidth
+                windowFrame.origin.x = screenFrame.origin.x
+                windowFrame.origin.y = screenFrame.origin.y + (mainPaneWindowHeight * CGFloat(acc.count))
+                windowFrame.size.width = mainPaneWindowWidth
+                windowFrame.size.height = mainPaneWindowHeight
+            } else {
+                scaleFactor = screenFrame.size.width / secondaryPaneWindowWidth
+                windowFrame.origin.x = screenFrame.origin.x + mainPaneWindowWidth
+                windowFrame.origin.y = screenFrame.origin.y
+                windowFrame.size.width = secondaryPaneWindowWidth
+                windowFrame.size.height = secondaryPaneWindowHeight
+            }
+
+            let resizeRules = ResizeRules(isMain: isMain, unconstrainedDimension: .horizontal, scaleFactor: scaleFactor)
+            let frameAssignment = FrameAssignment<Window>(
+                frame: windowFrame,
+                window: window,
+                screenFrame: screenFrame,
+                resizeRules: resizeRules
+            )
+
+            assignments.append(FrameAssignmentOperation(frameAssignment: frameAssignment, windowSet: windowSet))
+
+            return assignments
+        }
+    }
+}
--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source "https://rubygems.org"
 
-gem 'cocoapods', '= 1.10.0.rc.1'
+gem 'cocoapods', '= 1.10.1'
 gem 'fastlane'
 gem 'xcpretty'
